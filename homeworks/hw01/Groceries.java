04c78b759dfc1b15fa8a9d9b4f4c38cf94362cf8
Shriya Sai Shivakumar <sshivakuma@goldengate.cs.brynmawr.edu>
2024-02-07 21:31:48 -0500

    Question1

--- homeworks/hw01/Groceries.java
+++ homeworks/hw01/Groceries.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
   public class Groceries {
   public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+
+	  Scanner sc = new Scanner(System.in);
 
 System.out.print("How many times do you go to the grocery store each week? ");
 int numTrips = sc.nextInt();
@@ -13,9 +14,6 @@ int numTrips = sc.nextInt();
 System.out.print("How much do you spend on each trip? ");
 double costPerTrip = sc.nextDouble();
 
-
-System.out.print("How much do you spend on each trip? ");
-double costPerTrip = sc.nextDouble();
 
 double totalCost = numTrips * costPerTrip;
 
